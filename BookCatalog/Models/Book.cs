17fa47ce663c9169e82a1c7c0868e75983864963
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-08-16 07:11:16 -0400

    scaffolding

--- BookCatalog/Models/Book.cs
+++ BookCatalog/Models/Book.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace BookCatalog.Models
 {
     public class Book
     {
-        public Book()
-        {
-        }
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+
     }
 }
 
