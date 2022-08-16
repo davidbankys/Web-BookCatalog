0670b2236b8a0964660182ba22f52ff708e6ce9a
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-08-15 20:45:11 -0400

    scaffolding added

--- BookCatalog/Program.cs
+++ BookCatalog/Program.cs
@@ -1,7 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BookCatalog.Data;
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<BookCatalogContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("BookCatalogContext") ?? throw new InvalidOperationException("Connection string 'BookCatalogContext' not found.")));
 
 var app = builder.Build();
 
